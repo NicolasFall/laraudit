7acbf8e41827761b39a6fcc0e35e5ae5b9253b86
nicolas dandrea <nicolas.dandrea@osep.mendoza.gov.ar>
2018-03-27 11:28:11 -0300

    [Added] configuration to publish vendor assets and files.

--- src/providers/AuditServiceProvider.php
+++ src/providers/AuditServiceProvider.php
@@ -17,13 +17,12 @@ class AuditServiceProvider extends ServiceProvider
         $this->app->singleton(Auditor::class, function ($app) {
             return new Auditor();
         });
+    }
 
+    public function boot(){
         $this->publishes([
             __DIR__.'/../config/audit.php' => config_path('audit.php'),
-        ]);
-
-        $this->publishes([
-            __DIR__.'/../migrations/create_audit_table.php' => database_path('audit.php'),
+            __DIR__.'/../migrations/create_audit_table.php' => database_path('create_audit_table.php')
         ]);
     }
 }
