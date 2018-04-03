cd3b0f6c0f31e73edc4139281632ae5f874a4f3e
nicolasfall <dandrea.nicolas.ack@gmail.com>
2018-04-03 20:19:29 -0300

    .

--- src/Classes/AuditFacade.php
+++ src/Classes/AuditFacade.php
@@ -13,14 +13,18 @@ class AuditFacade extends Facade
 {
     protected const FACADE_NAME = 'Audit';
     protected static $auditor = null;
-
+    
+    public function __construct(Auditor $auditor)
+    {
+        self::$auditor = $auditor;
+    }
     protected static function getFacadeAccessor()
     {
         return static::FACADE_NAME;
     }
 
-    protected static function log(Auditor $auditor, Model $model, $user = null)
+    protected static function log(Model $model, $user = null)
     {
-        $this->auditor->log_changes($model, $user);
+        self::$auditor->log_changes($model, $user);
     }
 }
